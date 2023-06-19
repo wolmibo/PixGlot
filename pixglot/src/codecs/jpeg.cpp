0f927a63f41e039324f9d684e1b9e10712f88e18
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-06-19 18:52:19 +0200

    Jpeg: Add EXIF decoder

--- pixglot/src/codecs/jpeg.cpp
+++ pixglot/src/codecs/jpeg.cpp
@@ -19,6 +19,178 @@ namespace {
 
 
 
+  class exif_decoder {
+    public:
+      [[nodiscard]] static bool is_exif(std::span<const std::byte> buffer) {
+        return buffer.size() > magic.size()
+          && std::ranges::equal(magic, buffer.subspan(0, magic.size()));
+      }
+
+
+
+      explicit exif_decoder(std::span<const std::byte> buffer)
+        : buffer_{buffer}
+      {
+        if (!is_exif(buffer_)) {
+          throw decode_error{codec::jpeg, "exif: invalid file format"};
+        }
+        buffer_ = buffer.subspan(0, magic.size());
+
+        read_tiff();
+      }
+
+
+
+    private:
+      static constexpr std::array<std::byte, 6> magic {
+        std::byte{'E'}, std::byte{'X'}, std::byte{'I'}, std::byte{'F'},
+        std::byte{0}, std::byte{0}
+      };
+
+      static constexpr std::array<std::byte, 2> little_e{std::byte{'I'}, std::byte{'I'}};
+      static constexpr std::array<std::byte, 2> big_e   {std::byte{'M'}, std::byte{'M'}};
+
+
+
+      std::span<const std::byte> buffer_;
+      std::endian                byte_order_{std::endian::native};
+
+
+
+      enum class value_type : uint16_t {
+        byte      = 1,
+        ascii     = 2,
+        ushort    = 3,
+        ulong     = 4,
+        rational  = 5,
+        undefined = 7,
+        slong     = 8,
+        srational = 10,
+      };
+
+      using ifd_value = std::variant<
+        std::array<uint8_t, 4>,
+        std::array<uint16_t, 2>,
+        uint32_t,
+        int32_t,
+        std::pair<value_type, size_t>
+      >;
+
+      struct ifd_entry {
+        uint16_t  tag;
+        uint32_t  count;
+        ifd_value value;
+      };
+
+
+
+
+
+      [[nodiscard]] ifd_entry read_ifd_entry(size_t offset) {
+        return ifd_entry{
+          .tag   = int_at<uint16_t>(offset),
+          .count = int_at<uint32_t>(offset + 4),
+          .value = read_ifd_value(offset + 8, int_at<uint16_t>(offset + 2)),
+        };
+      }
+
+
+
+      [[nodiscard]] ifd_value read_ifd_value(size_t offset, uint16_t type) {
+        switch (static_cast<value_type>(type)) {
+          case value_type::byte:
+            return std::array<uint8_t, 4> {
+              int_at<uint8_t>(offset + 3),
+              int_at<uint8_t>(offset + 2),
+              int_at<uint8_t>(offset + 1),
+              int_at<uint8_t>(offset + 0)
+            };
+
+          case value_type::ushort:
+            return std::array<uint16_t, 2> {
+              int_at<uint16_t>(offset + 2),
+              int_at<uint16_t>(offset + 0)
+            };
+
+          case value_type::ulong:
+            return int_at<uint32_t>(offset);
+          case value_type::slong:
+            return int_at<int32_t>(offset);
+
+          case value_type::ascii:
+          case value_type::rational:
+          case value_type::undefined:
+          case value_type::srational:
+          default: {
+            auto off = int_at<uint32_t>(offset);
+            if (off >= buffer_.size()) {
+              off = 0;
+            }
+            return std::make_pair(static_cast<value_type>(type), off);
+          }
+        }
+      }
+
+
+
+      void load_entries(size_t offset) {
+        while (offset != 0) {
+          auto count = int_at<uint16_t>(offset);
+          offset += 2;
+          for (uint16_t i = 0; i < count; ++i) {
+            offset += 12;
+          }
+          offset = int_at<uint32_t>(offset);
+        }
+      }
+
+
+
+      void read_tiff() {
+        if (buffer_.size() < 8) {
+          throw decode_error{codec::jpeg, "exif: incomplete tiff header"};
+        }
+
+        if (std::ranges::equal(buffer_.subspan(0, little_e.size()), little_e)) {
+          byte_order_ = std::endian::little;
+        } else if (std::ranges::equal(buffer_.subspan(0, big_e.size()), big_e)) {
+          byte_order_ = std::endian::big;
+        }
+
+        if (int_at<uint16_t>(2) != 42) {
+          throw decode_error{codec::jpeg, "exif: tiff: wrong byte order"};
+        }
+
+        load_entries(int_at<uint32_t>(4));
+      }
+
+
+
+
+
+      template<typename T>
+      [[nodiscard]] T int_at(size_t offset) {
+        if (offset > buffer_.size() - sizeof(T)) {
+          throw decode_error{codec::jpeg, "exif: unexpected eof"};
+        }
+
+        if constexpr (sizeof(T) > 1) {
+          T value{};
+          std::ranges::copy(buffer_.subspan(offset, sizeof(T)),
+              std::as_writable_bytes(std::span{&value, 1}).begin());
+
+          if (byte_order_ == std::endian::native) {
+            return value;
+          }
+          return std::byteswap(value);
+        } else {
+          return static_cast<T>(buffer_[offset]);
+        }
+      }
+  };
+
+
+
 #if 0
   [[nodiscard]] constexpr optional<square_isometry> convert_orientation(uint32_t or) {
     using enum square_isometry;
@@ -300,8 +472,8 @@ namespace {
         std::vector<std::byte> buffer(std::byteswap(length_be) - 2);
         source_mgr::get(cinfo).fill(buffer);
 
-        if (cinfo->unread_marker == JPEG_APP1) {
-
+        if (cinfo->unread_marker == JPEG_APP1 && exif_decoder::is_exif(buffer)) {
+          exif_decoder exif{buffer};
         }
 
         return TRUE;
