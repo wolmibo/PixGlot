9f81eff9d1bdf0403e225b62d95baf1f4d9079d8
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-06-23 16:22:42 +0200

    GL: Set texture wrapping to CLAMP_TO_BORDER

--- pixglot/src/gl-texture.cpp
+++ pixglot/src/gl-texture.cpp
@@ -31,8 +31,8 @@ namespace {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
     pixglot::utils::gl_swizzle_mask(format.channels);
 
