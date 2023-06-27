07ca3946f88799bfaa7b973136cf4faded4a82e3
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-06-27 17:06:42 +0200

    Catch non-pixglot exceptions

--- pixglot/src/decode.cpp
+++ pixglot/src/decode.cpp
@@ -42,36 +42,44 @@ image pixglot::decode(
     progress_access_token pat,
     output_format         fmt
 ) {
-  decoder dec{r, std::move(pat), fmt};
+  try {
+    decoder dec{r, std::move(pat), fmt};
 
-  switch (c) {
+    switch (c) {
 #ifdef PIXGLOT_WITH_JPEG
-    case codec::jpeg: return decode_jpeg(std::move(dec));
+      case codec::jpeg: return decode_jpeg(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_PNG
-    case codec::png:  return decode_png(std::move(dec));
+      case codec::png:  return decode_png(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_AVIF
-    case codec::avif: return decode_avif(std::move(dec));
+      case codec::avif: return decode_avif(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_EXR
-    case codec::exr:  return decode_exr(std::move(dec));
+      case codec::exr:  return decode_exr(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_PPM
-    case codec::ppm:  return decode_ppm(std::move(dec));
+      case codec::ppm:  return decode_ppm(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_WEBP
-    case codec::webp: return decode_webp(std::move(dec));
+      case codec::webp: return decode_webp(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_GIF
-    case codec::gif:  return decode_gif(std::move(dec));
+      case codec::gif:  return decode_gif(std::move(dec));
 #endif
 #ifdef PIXGLOT_WITH_JXL
-    case codec::jxl:  return decode_jxl(std::move(dec));
+      case codec::jxl:  return decode_jxl(std::move(dec));
 #endif
-    default:
-      throw no_decoder{};
+      default: break;
+    }
+  } catch (pixglot::base_exception&) {
+    throw;
+  } catch (std::exception& ex) {
+    throw base_exception{std::string{"fatal error: "} + ex.what() +
+      "\n(this is most likely a bug or a problem outside of the control of pixglot)"};
   }
+
+  throw no_decoder{};
 }
 
 
