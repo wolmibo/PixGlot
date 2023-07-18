ce25d893b5b77287fb105c6eab729cec2ba4f665
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-07-18 16:20:07 +0200

    Inspect: Cleanup

--- examples/inspect.cpp
+++ examples/inspect.cpp
@@ -2,7 +2,6 @@
 #include <chrono>
 #include <iomanip>
 #include <iostream>
-#include <numeric>
 
 #include <pixglot/decode.hpp>
 #include <pixglot/frame.hpp>
@@ -35,15 +34,17 @@ std::string_view str(std::endian endian) {
 
 
 
-void print_ipi(const pixglot::frame_source_info& fsi) {
-  std::cout << pixglot::stringify(fsi.color_model());
+[[nodiscard]] std::string frame_source_format_to_string(
+    const pixglot::frame_source_info& fsi
+) {
+  std::string output{pixglot::stringify(fsi.color_model())};
 
   if (fsi.color_model() == pixglot::color_model::yuv) {
-    std::cout << pixglot::stringify(fsi.subsampling());
+    output += pixglot::stringify(fsi.subsampling());
   }
 
   if (fsi.has_alpha()) {
-    std::cout << 'a';
+    output.push_back('a');
   }
 
   bool uniform_color = !fsi.has_color() ||
@@ -51,25 +52,30 @@ void print_ipi(const pixglot::frame_source_info& fsi) {
       (fsi.color_model_format()[0] == fsi.color_model_format()[1] &&
       fsi.color_model_format()[1] == fsi.color_model_format()[2]);
 
-  std::cout << '_';
+  output.push_back('_');
 
   if (uniform_color) {
-    std::cout << pixglot::stringify(fsi.color_model_format()[0]);
+    output += pixglot::stringify(fsi.color_model_format()[0]);
   } else {
-    std::cout << pixglot::stringify(fsi.color_model_format()[0]) << '_';
-    std::cout << pixglot::stringify(fsi.color_model_format()[1]) << '_';
-    std::cout << pixglot::stringify(fsi.color_model_format()[2]);
+    output += pixglot::stringify(fsi.color_model_format()[0]);
+    output.push_back('_');
+    output += pixglot::stringify(fsi.color_model_format()[1]);
+    output.push_back('_');
+    output += pixglot::stringify(fsi.color_model_format()[2]);
   }
 
   if (fsi.has_alpha() &&
       (!uniform_color || fsi.color_model_format()[3] != fsi.color_model_format()[0])) {
 
     if (fsi.color_model_format()[3] == pixglot::data_source_format::index) {
-      std::cout << "_indexed α";
+      output += "_indexed α";
     } else {
-      std::cout << '_' << pixglot::stringify(fsi.color_model_format()[3]);
+      output.push_back('_');
+      output += pixglot::stringify(fsi.color_model_format()[3]);
     }
   }
+
+  return output;
 }
 
 
@@ -82,19 +88,10 @@ void print_image(const pixglot::image& image) {
     std::cout << "  ⚠ " << str << '\n';
   }
 
-  if (image.has_warnings()) {
-    std::cout << '\n';
-  }
-
   std::cout << "  animated:    " << str(image.animated()) << '\n';
   std::cout << "  frames:      " << image.size() << '\n';
 
-  if (!image.empty()) {
-    std::cout << '\n';
-  }
-
   for (const auto& f: image.frames()) {
-
     std::cout << "  • ";
 
     if (auto name = f.name()) {
@@ -103,18 +100,14 @@ void print_image(const pixglot::image& image) {
 
     std::cout << f.width() << "×" << f.height() << ", ";
 
-    std::cout << pixglot::to_string(f.format());
-    if (pixglot::byte_size(f.format().format) > 1) {
-      if (f.type() == pixglot::storage_type::pixel_buffer) {
-        std::cout << '(' << str(f.pixels().endian()) << "), ";
-      } else {
-        std::cout << "(native), ";
-      }
-    } else {
-      std::cout << ", ";
+    auto fsf = frame_source_format_to_string(f.source_info());
+    auto fmt = pixglot::to_string(f.format());
+    std::cout << fsf;
+    if (fsf != fmt) {
+      std::cout << "(→" << fmt << ")";
     }
 
-    std::cout << "γ=" << f.gamma();
+    std::cout << ", γ=" << f.gamma();
 
     if (f.orientation() != pixglot::square_isometry::identity) {
       std::cout << ", " << pixglot::to_string(f.orientation());
@@ -134,41 +127,79 @@ void print_image(const pixglot::image& image) {
       std::cout << ", " << f.duration().count() << "µs";
     }
 
-    std::cout << ", ";
-    print_ipi(f.source_info());
-
     std::cout << '\n';
   }
 
 
-  std::cout << std::flush;
+  std::cout << std::endl;
 }
 
 
 
 
+void print_help(const std::filesystem::path& name) {
+  std::cout <<
 
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    //NOLINTNEXTLINE(*pointer-arithmetic)
-    std::cout << "Usage: " << argv[0] << " <image>" << std::endl;
+    "Inspect the provided file(s) using the pixglot library and return the number of\n"
+    "errors encountered.\n"
+    "\n"
+    "Usage: " << name.filename().native() << " [options] <image1>...\n"
+    "\n"
+    "Available options:\n"
+    "  -h, --help              show this help and exit\n"
+
+    << std::flush;
+}
+
+
+
+int main(int argc, const char* argv[]) {
+  auto args = std::span{argv, static_cast<size_t>(argc)};
+
+  std::vector<std::filesystem::path> files;
+  files.reserve(args.size() - 1);
+
+  bool help = (args.size() == 1);
+
+  for (std::string_view arg: args.subspan(1)) {
+    if (arg == "-h" || arg == "--help") {
+      help = true;
+      continue;
+    }
+
+    files.emplace_back(arg);
   }
 
-  try {
-    pixglot::output_format output_format;
-    output_format.storage_type(pixglot::storage_type::no_pixels);
-    //
-    //NOLINTNEXTLINE(*pointer-arithmetic)
-    auto image{pixglot::decode(pixglot::reader{argv[1]}, {}, output_format)};
-
-    //NOLINTNEXTLINE(*pointer-arithmetic)
-    std::cout << std::filesystem::path{argv[1]}.filename().native()
-      << '\n' << std::endl;
-
-    print_image(image);
-
-  } catch (std::exception& ex) {
-    std::cerr << "***Error*** " << ex.what() << std::endl;
-    return 1;
+  if (help) {
+    print_help(args[0]);
+    return 0;
   }
+
+
+  int error_count{0};
+
+  pixglot::output_format output_format;
+  output_format.storage_type(pixglot::storage_type::no_pixels);
+
+
+  for (const auto& file: files) {
+    std::cout << file.native() << '\n';
+
+    try {
+      auto image{pixglot::decode(pixglot::reader{file}, {}, output_format)};
+
+
+      print_image(image);
+
+    } catch (const pixglot::base_exception& ex) {
+      std::cerr << "***Error*** " << ex.message() << std::endl;
+      error_count++;
+
+    } catch (std::exception& ex) {
+      std::cerr << "***Error*** " << ex.what() << std::endl;
+      error_count++;
+    }
+  }
+
+  return error_count;
 }
