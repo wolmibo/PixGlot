4d61935ff480855f2cdb1aeff6003b7d271d11d0
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-07-19 18:26:48 +0200

    PerfTest: Add options for complete output_format

--- examples/perftest.cpp
+++ examples/perftest.cpp
@@ -1,3 +1,4 @@
+#include "pixglot/square-isometry.hpp"
 #include <chrono>
 #include <iomanip>
 #include <iostream>
@@ -108,6 +109,92 @@ void print_time_tree(std::span<const pixglot::frame> frames) {
 
 
 
+std::ostream& operator<<(std::ostream& out, const std::source_location& location) {
+  out << location.file_name() << ':' << location.line() << ':' << location.column();
+  return out;
+}
+
+
+
+
+
+//NOLINTBEGIN(*-macro-usage)
+#define STR_TO_ENTRY(x) if ( str == #x ) return x;
+#define BEGIN_STR_TO_ENUM(x, y) \
+  [[nodiscard]] x::y y##_from_string(std::string_view str) {\
+    using enum x::y;
+#define END_STR_TO_ENUM(enum) \
+    throw std::runtime_error{"unknown " #enum ": " + std::string{str}}; \
+  }
+//NOLINTEND(*-macro-usage)
+
+
+
+BEGIN_STR_TO_ENUM(pixglot, storage_type)
+  STR_TO_ENTRY(no_pixels);
+  STR_TO_ENTRY(pixel_buffer);
+  STR_TO_ENTRY(gl_texture);
+END_STR_TO_ENUM(storage_type)
+
+
+
+BEGIN_STR_TO_ENUM(pixglot, square_isometry)
+  STR_TO_ENTRY(identity);
+  STR_TO_ENTRY(flip_x);
+  STR_TO_ENTRY(rotate_cw);
+  STR_TO_ENTRY(rotate_half);
+  STR_TO_ENTRY(rotate_ccw);
+  STR_TO_ENTRY(flip_y);
+  STR_TO_ENTRY(transpose);
+  STR_TO_ENTRY(anti_transpose);
+END_STR_TO_ENUM(square_isometry)
+
+
+
+BEGIN_STR_TO_ENUM(pixglot, alpha_mode)
+  STR_TO_ENTRY(none);
+  STR_TO_ENTRY(straight);
+  STR_TO_ENTRY(premultiplied);
+END_STR_TO_ENUM(alpha_mode);
+
+
+
+BEGIN_STR_TO_ENUM(pixglot, data_format)
+  STR_TO_ENTRY(u8);
+  STR_TO_ENTRY(u16);
+  STR_TO_ENTRY(u32);
+  STR_TO_ENTRY(f16);
+  STR_TO_ENTRY(f32);
+END_STR_TO_ENUM(data_format)
+
+
+
+BEGIN_STR_TO_ENUM(std, endian)
+  STR_TO_ENTRY(native);
+  STR_TO_ENTRY(little);
+  STR_TO_ENTRY(big);
+END_STR_TO_ENUM(endian)
+
+
+
+[[nodiscard]] float float_from_string(std::string_view str) {
+  if (str.empty()) {
+    throw std::runtime_error{"expected float"};
+  }
+
+  float out{1.f};
+  auto res = std::from_chars(str.begin(), str.end(), out);
+  if (res.ec == std::errc::invalid_argument || res.ec == std::errc::result_out_of_range) {
+    throw std::runtime_error{"invalid float format: " + std::string{str}};
+  }
+  return out;
+}
+
+
+
+
+
+
 void print_help(const std::filesystem::path& name) {
   std::cout <<
 
@@ -116,66 +203,107 @@ void print_help(const std::filesystem::path& name) {
     "Usage: " << name.filename().native() << " [options] <image>\n"
     "\n"
     "Available options:\n"
-    "  -h, --help                             show this help and exit\n"
+    "  -h, --help                         show this help and exit\n"
     "\n"
-    "  -t, --prefer-target=<target>           set the preferred output target\n"
-    "  -T, --target=<target>                  set the output target\n"
+    "  --target=<target>                  set the target\n"
+    "  --orientation=<ori>                set the orientation\n"
+    "  --alpha-mode=<am>                  set the alpha-mode\n"
+    "  --data-format=<df>                 set the data-format\n"
+    "  --endian=<endian>                  set the endian\n"
+    "  --gamma=<gamma>                    set the gamma\n"
+    "  --fill-alpha                       fill missing alpha channel\n"
+    "  --expand-gray                      expand gray to rgb\n"
+    "\n"
+    "  --prefer-target=<target>           set the preferred target\n"
+    "  --prefer-orientation=<ori>         set the preferred orientation\n"
+    "  --prefer-alpha-mode=<am>           set the preferred alpha-mode\n"
+    "  --prefer-data-format=<df>          set the preferred data-format\n"
+    "  --prefer-endian=<endian>           set the preferred endian\n"
+    "  --prefer-gamma=<gamma>             set the preferred gamma\n"
+    "  --prefer-fill-alpha                prefer to fill missing alpha channel\n"
+    "  --prefer-expand-gray               prefer to expand gray to rgb\n"
+    "\n"
+    "  --enforce                          enforce the requested format\n"
+    "  --standard-format                  standard format (rgba_u8, all trafos applied)\n"
     "\n"
     "Enum values:\n"
-    "  <target>: no-pixels, pixel-buffer, gl-texture\n"
+    "  <target>:   no_pixels, pixel_buffer, gl_texture\n"
+    "  <ori>:      identity, flip_x, rotate_cw, rotate_half, rotate_ccw, flip_y,\n"
+    "              transpose, anti_transpose\n"
+    "  <am>:       none, straigh, premultiplied\n"
+    "  <df>:       u8, u16, u32, f16, f32\n"
+    "  <endian>:   native, little, big\n"
 
     << std::flush;
 }
 
 
 
-std::ostream& operator<<(std::ostream& out, const std::source_location& location) {
-  out << location.file_name() << ':' << location.line() << ':' << location.column();
-  return out;
-}
-
-
-
-[[nodiscard]] pixglot::storage_type storage_type_from_string(std::string_view str) {
-  if (str == "no-pixels"   ) return pixglot::storage_type::no_pixels;
-  if (str == "pixel-buffer") return pixglot::storage_type::pixel_buffer;
-  if (str == "gl-texture"  ) return pixglot::storage_type::gl_texture;
-
-  throw std::runtime_error{"unknown storage type " + std::string{str}};
-}
-
 
 
 int main(int argc, char** argv) {
   auto args = std::span{argv, static_cast<size_t>(argc)};
 
-  bool help{false};
-  pixglot::output_format output_format;
+  static std::array<option, 20> long_options = {
+    option{"help",                 no_argument,       nullptr, 'h'},
 
-  static std::array<option, 5> long_options = {
-    option{"help",          no_argument,       nullptr, 'h'},
-    option{"prefer-target", required_argument, nullptr, 't'},
-    option{"target",        required_argument, nullptr, 'T'},
-    option{nullptr,         0,                 nullptr, 0},
+    option{"target",               required_argument, nullptr, 1000},
+    option{"orientation",          required_argument, nullptr, 1001},
+    option{"alpha-mode",           required_argument, nullptr, 1002},
+    option{"data-format",          required_argument, nullptr, 1003},
+    option{"endian",               required_argument, nullptr, 1004},
+    option{"gamma",                required_argument, nullptr, 1005},
+    option{"fill-alpha",           no_argument,       nullptr, 1006},
+    option{"expand-gray",          no_argument,       nullptr, 1007},
+
+    option{"prefer-target",        required_argument, nullptr, 2000},
+    option{"prefer-orientation",   required_argument, nullptr, 2001},
+    option{"prefer-alpha-mode",    required_argument, nullptr, 2002},
+    option{"prefer-data-format",   required_argument, nullptr, 2003},
+    option{"prefer-endian",        required_argument, nullptr, 2004},
+    option{"prefer-gamma",         required_argument, nullptr, 2005},
+    option{"prefer-fill-alpha",    no_argument,       nullptr, 2006},
+    option{"prefer-expand-gray",   no_argument,       nullptr, 2007},
+
+    option{"enforce",              no_argument,       nullptr, 3001},
+    option{"standard-format",      no_argument,       nullptr, 3002},
+    option{nullptr,                0,                 nullptr, 0},
   };
 
   auto pref = pixglot::preference_level::prefer;
 
+  pixglot::output_format of;
+
+  bool help           {false};
+  bool enforce        {false};
+  bool standard_format{false};
+
   int c{-1};
-  while ((c = getopt_long(args.size(), args.data(), "ht:T:",
+  while ((c = getopt_long(args.size(), args.data(), "h",
                           long_options.data(), nullptr)) != -1) {
     switch (c) {
-      case 'h':
-        help = true;
-        break;
-      case 't':
-        if (optarg == nullptr) throw std::runtime_error{"missing argument"};
-        output_format.storage_type(storage_type_from_string(optarg));
-        break;
-      case 'T':
-        if (optarg == nullptr) throw std::runtime_error{"missing argument"};
-        output_format.storage_type({storage_type_from_string(optarg), pref});
-        break;
+      case 'h': help = true; break;
+
+      case 1000: of.storage_type(storage_type_from_string(optarg));    break;
+      case 1001: of.orientation (square_isometry_from_string(optarg)); break;
+      case 1002: of.alpha_mode  (alpha_mode_from_string(optarg));      break;
+      case 1003: of.data_format (data_format_from_string(optarg));     break;
+      case 1004: of.endian      (endian_from_string(optarg));          break;
+      case 1005: of.gamma       (float_from_string(optarg));           break;
+      case 1006: of.fill_alpha        (true); break;
+      case 1007: of.expand_gray_to_rgb(true); break;
+
+      case 2000: of.storage_type({storage_type_from_string(optarg), pref});    break;
+      case 2001: of.orientation ({square_isometry_from_string(optarg), pref}); break;
+      case 2002: of.alpha_mode  ({alpha_mode_from_string(optarg), pref});      break;
+      case 2003: of.data_format ({data_format_from_string(optarg), pref});     break;
+      case 2004: of.endian      ({endian_from_string(optarg), pref});          break;
+      case 2005: of.gamma       ({float_from_string(optarg), pref});           break;
+      case 2006: of.fill_alpha        ({true, pref}); break;
+      case 2007: of.expand_gray_to_rgb({true, pref}); break;
+
+      case 3001: enforce         = true; break;
+      case 3002: standard_format = true; break;
     }
   }
 
@@ -189,6 +317,16 @@ int main(int argc, char** argv) {
     return 0;
   }
 
+  if (standard_format) {
+    of = pixglot::output_format::standard();
+  }
+
+  if (enforce) {
+    of.enforce();
+  }
+
+
+
   try {
     pixglot::progress_token tok;
     tok.frame_callback      ([](auto&&) { emit_event(event::frame_finish);   });
@@ -198,7 +336,7 @@ int main(int argc, char** argv) {
     auto at = tok.access_token();
 
     emit_event(event::image_begin);
-    auto image{pixglot::decode(reader, std::move(at), output_format)};
+    auto image{pixglot::decode(reader, std::move(at), of)};
     emit_event(event::image_finish);
 
     for (const auto& warn: image.warnings()) {
