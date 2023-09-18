196687397fad3a041c1fc1922f9b5fcd85801d29
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-09-18 17:53:39 +0200

    CPU-Conv: Apply transformation

--- pixglot/src/conversions-cpu.cpp
+++ pixglot/src/conversions-cpu.cpp
@@ -3,13 +3,29 @@
 
 
 namespace pixglot::details {
+  void apply_orientation(pixel_buffer&, square_isometry);
+
+
+
   void convert(
-      pixel_buffer&              /*pixels*/,
+      pixel_buffer&              pixels,
       std::optional<std::endian> /*target_endian*/,
-      pixel_format               /*target_format*/,
+      pixel_format               target_format,
       int                        /*premultiply*/,
       float                      /*gamma_diff*/,
-      square_isometry            /*transform*/
+      square_isometry            transform
   ) {
+    if (transform != square_isometry::identity
+        && pixels.format().size() < target_format.size()) {
+      apply_orientation(pixels, transform);
+      transform = square_isometry::identity;
+    }
+
+
+
+
+    if (transform != square_isometry::identity) {
+      apply_orientation(pixels, transform);
+    }
   }
 }
