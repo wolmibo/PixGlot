2bd2f045266cb7e17a9c81767d0ea36b39868b8b
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-09-19 15:52:11 +0200

    Share endian related functions

--- pixglot/src/conversions-cpu-endian.cpp
+++ pixglot/src/conversions-cpu-endian.cpp
@@ -12,22 +12,42 @@ namespace {
       x = std::byteswap(x);
     }
   }
-
-
-
-  [[nodiscard]] std::endian swap(std::endian endian) {
-    if (endian == std::endian::little) {
-      return std::endian::big;
-    }
-    return std::endian::little;
-  }
 }
 
 
 
 namespace pixglot::details {
+  [[nodiscard]] std::endian swap_endian(std::endian endian) {
+    if (endian == std::endian::little) {
+      return std::endian::big;
+    }
+    return std::endian::little;
+  }
+
+
+
+  void swap_bytes(std::span<std::byte> buffer, size_t chunk_size) {
+    if (chunk_size == 1) {
+      return;
+    }
+
+    if (chunk_size == 2) {
+      ::swap_bytes<pixglot::u16>(buffer);
+    } else if (chunk_size == 4) {
+      ::swap_bytes<pixglot::u32>(buffer);
+    } else {
+      throw pixglot::base_exception{
+        "Unable to swap bytes",
+        "No byte-swapping is implemented for data_format with byte_size == " +
+          std::to_string(chunk_size)
+      };
+    }
+  }
+
+
+
   void apply_byte_swap(pixglot::pixel_buffer& pb) {
-    pb.endian(swap(pb.endian()));
+    pb.endian(swap_endian(pb.endian()));
 
     if (byte_size(pb.format().format) < 2) {
       return;
@@ -35,16 +55,6 @@ namespace pixglot::details {
 
     static_assert(pixglot::pixel_buffer::padding() % 4 == 0);
 
-    if (byte_size(pb.format().format) == 2) {
-      swap_bytes<pixglot::u16>(pb.data());
-    } else if (byte_size(pb.format().format) == 4) {
-      swap_bytes<pixglot::u32>(pb.data());
-    } else {
-      throw pixglot::base_exception{
-        "Unable to swap bytes",
-        "No byte-swapping is implemented for data_format with byte_size == " +
-          std::to_string(byte_size(pb.format().format))
-      };
-    }
+    swap_bytes(pb.data(), byte_size(pb.format().format));
   }
 }
