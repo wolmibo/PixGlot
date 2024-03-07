26e1282107fe8be1d8c7b29bfd6c4da00e573218
wolmibo <102993087+wolmibo@users.noreply.github.com>
2024-03-07 15:13:53 +0100

    Cleanup: Add braces around if body

--- pixglot/src/codecs.cpp
+++ pixglot/src/codecs.cpp
@@ -10,30 +10,30 @@ using namespace pixglot;
 std::optional<codec> pixglot::determine_codec(
   [[maybe_unused]] std::span<const std::byte> input
 ) {
-  if (false) {}
+  if (false) {} // NOLINT(*boolean-expr)
 #ifdef PIXGLOT_WITH_JPEG
-  else if (magic<codec::jpeg>::check(input)) return codec::jpeg;
+  else if (magic<codec::jpeg>::check(input)) { return codec::jpeg; }
 #endif
 #ifdef PIXGLOT_WITH_PNG
-  else if (magic<codec::png>::check(input)) return codec::png;
+  else if (magic<codec::png>::check(input))  { return codec::png;  }
 #endif
 #ifdef PIXGLOT_WITH_AVIF
-  else if (magic<codec::avif>::check(input)) return codec::avif;
+  else if (magic<codec::avif>::check(input)) { return codec::avif; }
 #endif
 #ifdef PIXGLOT_WITH_EXR
-  else if (magic<codec::exr>::check(input)) return codec::exr;
+  else if (magic<codec::exr>::check(input))  { return codec::exr;  }
 #endif
 #ifdef PIXGLOT_WITH_PPM
-  else if (magic<codec::ppm>::check(input)) return codec::ppm;
+  else if (magic<codec::ppm>::check(input))  { return codec::ppm;  }
 #endif
 #ifdef PIXGLOT_WITH_WEBP
-  else if (magic<codec::webp>::check(input)) return codec::webp;
+  else if (magic<codec::webp>::check(input)) { return codec::webp; }
 #endif
 #ifdef PIXGLOT_WITH_GIF
-  else if (magic<codec::gif>::check(input)) return codec::gif;
+  else if (magic<codec::gif>::check(input))  { return codec::gif;  }
 #endif
 #ifdef PIXGLOT_WITH_JXL
-  else if (magic<codec::jxl>::check(input)) return codec::jxl;
+  else if (magic<codec::jxl>::check(input))  { return codec::jxl;  }
 #endif
 
   return {};
