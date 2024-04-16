wolmibo/PixGlot

commit f51bc808c19a1d53966b5782cbf059d70377a54c

Reader: Hide implementation

pixglot/src/reader.cpp
@@ -1,0 +1,111 @@
+#include "pixglot/reader.hpp"
+
+using namespace pixglot;
+
+
+
+class reader::impl {
+  public:
+    FILE* fptr;
+
+
+
+    impl(FILE* f) : fptr{f} {}
+
+    impl(const impl&) = delete;
+    impl(impl&&)      = delete;
+
+    impl& operator=(const impl&) = delete;
+    impl& operator=(impl&&) = delete;
+
+    ~impl() {
+      fclose(fptr); //NOLINT(*-owning-memory)
+    }
+};
+
+
+
+
+
+reader::reader(reader&&) noexcept = default;
+
+reader& reader::operator=(reader&&) noexcept = default;
+
+reader::~reader() = default;
+
+
+
+reader::reader(const std::filesystem::path& p) :
+  //NOLINTNEXTLINE(*-owning-memory)
+  impl_{std::make_unique<impl>(fopen(p.c_str(), "rb"))}
+{
+  if (impl_->fptr == nullptr) {
+    throw no_stream_access{p.string()};
+  }
+}
+
+
+
+
+
+size_t reader::read(std::span<std::byte> buffer) {
+  if (impl_->fptr != nullptr) {
+    return fread(buffer.data(), sizeof(std::byte), buffer.size(), impl_->fptr);
+  }
+  return 0;
+}
+
+
+
+size_t reader::peek(std::span<std::byte> buffer) const {
+  if (impl_->fptr != nullptr) {
+    size_t current = ftell(impl_->fptr);
+    size_t count   = fread(buffer.data(), sizeof(std::byte), buffer.size(), impl_->fptr);
+    fseek(impl_->fptr, current, SEEK_SET);
+    return count;
+  }
+  return 0;
+}
+
+
+
+
+
+void reader::skip(size_t count) {
+  if (impl_->fptr != nullptr) {
+    fseek(impl_->fptr, count, SEEK_CUR);
+  }
+}
+
+
+
+size_t reader::position() const {
+  return ftell(impl_->fptr);
+}
+
+
+
+size_t reader::size() const {
+  size_t current = ftell(impl_->fptr);
+  fseek(impl_->fptr, 0, SEEK_END);
+  size_t size = ftell(impl_->fptr);
+  fseek(impl_->fptr, current, SEEK_SET);
+  return size;
+}
+
+
+
+bool reader::seek(size_t pos) {
+  if (impl_->fptr != nullptr) {
+    return fseek(impl_->fptr, pos, SEEK_SET) == 0;
+  }
+  return false;
+}
+
+
+
+
+
+bool reader::eof() const {
+  return (impl_->fptr != nullptr) && feof(impl_->fptr) != 0;
+}

pixglot/include/pixglot/reader.hpp
@@ -17,88 +17,35 @@ namespace pixglot {
 
 class reader {
   public:
-    explicit reader(const std::filesystem::path& p) :
-      fptr_{fopen(p.string().c_str(), "rb"), &fclose}
-    {
-      if (!fptr_) {
-        throw no_stream_access{p.string()};
-      }
-    }
+    explicit reader(const std::filesystem::path&);
+
+    reader(const reader&) = delete;
+    reader(reader&&) noexcept;
+
+    reader& operator=(const reader&) = delete;
+    reader& operator=(reader&&) noexcept;
+
+    ~reader();
 
 
 
-    [[nodiscard]] size_t read(std::span<std::byte> buffer) {
-      if (fptr_) {
-        return fread(buffer.data(), sizeof(std::byte), buffer.size(), fptr_.get());
-      }
-      return 0;
-    }
+    [[nodiscard]] size_t peek(std::span<std::byte>) const;
+    [[nodiscard]] size_t read(std::span<std::byte>);
+
+    void skip(size_t);
+    [[nodiscard]] bool seek(size_t);
+
+    [[nodiscard]] bool   eof()      const;
+    [[nodiscard]] size_t position() const;
+    [[nodiscard]] size_t size()     const;
 
 
-
-    [[nodiscard]] size_t read(std::span<std::byte> buffer, size_t offset) {
-      if (fptr_ && fseek(fptr_.get(), offset, SEEK_SET) == 0) {
-        return fread(buffer.data(), sizeof(std::byte), buffer.size(), fptr_.get());
-      }
-      return 0;
-    }
-
-
-
-    [[nodiscard]] size_t peek(std::span<std::byte> buffer) const {
-      if (fptr_) {
-        size_t current = ftell(fptr_.get());
-        size_t count
-                 = fread(buffer.data(), sizeof(std::byte), buffer.size(), fptr_.get());
-        fseek(fptr_.get(), current, SEEK_SET);
-        return count;
-      }
-      return 0;
-    }
-
-
-
-    void skip(size_t count) {
-      if (fptr_) {
-        fseek(fptr_.get(), count, SEEK_CUR);
-      }
-    }
-
-
-
-    [[nodiscard]] size_t position() const {
-      return ftell(fptr_.get());
-    }
-
-
-
-    [[nodiscard]] size_t size() const {
-      size_t current = ftell(fptr_.get());
-      fseek(fptr_.get(), 0, SEEK_END);
-      size_t size = ftell(fptr_.get());
-      fseek(fptr_.get(), current, SEEK_SET);
-      return size;
-    }
-
-
-
-    void seek(size_t pos) {
-      if (fptr_) {
-        fseek(fptr_.get(), pos, SEEK_SET);
-      }
-    }
-
-
-
-    [[nodiscard]] bool eof() const {
-      return fptr_ &&
-        feof(fptr_.get()) != 0;
-    }
 
 
 
   private:
-    std::unique_ptr<FILE, decltype(&fclose)> fptr_;
+    class impl;
+    std::unique_ptr<impl> impl_;
 };
 
 }

pixglot/meson.build
@@ -16,6 +16,7 @@ sources = [
   'src/pixel-buffer.cpp',
   'src/pixel-format.cpp',
   'src/progress-token.cpp',
+  'src/reader.cpp',
   'src/square-isometry.cpp',
 ]
 

pixglot/src/codecs/avif.cpp
@@ -60,7 +60,10 @@ namespace {
 
         self->buffer_.resize(size);
 
-        out->size = self->input_->read(self->buffer_, offset);
+        if (!self->input_->seek(offset)) {
+          return AVIF_RESULT_IO_ERROR;
+        }
+        out->size = self->input_->read(self->buffer_);
         out->data = utils::byte_pointer_cast<const uint8_t>(self->buffer_.data());
 
         if (out->size == 0) {

commit dc755ea7549c51a8d32dc9d461d3019ef56a04c9

Fix null-deref on fopen failure
Was introduced in f51bc80

pixglot/src/reader.cpp
@@ -22,7 +22,9 @@ class reader::impl {
     impl& operator=(impl&&) = delete;
 
     ~impl() {
-      fclose(fptr); //NOLINT(*-owning-memory)
+      if (fptr != nullptr) {
+        fclose(fptr); //NOLINT(*-owning-memory)
+      }
     }
 };
 